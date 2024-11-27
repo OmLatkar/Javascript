1a188a28c806fbbc942a477d2d50f116da25bd59
Om_Latkar <63500839+OmLatkar@users.noreply.github.com>
2024-11-27 15:15:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+let score="33"
+let score1="33abc"
+let score2=null
+let score3=undefined
+let score4=true
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+let value1=Number(score1)
+let value2=Number(score2)
+let value3=Number(score3)
+let value4=Number(score4)
+console.log(typeof valueInNumber)
+console.log(value1);
+console.log(value2);
+console.log(value3);
+console.log(value4);
+
+//"33"=>33
+//"33abc"=>NaN (not a number; but its type is number)
+//true=>1; false=>0
+
+let isLoggedIn=1
+let x=""
+let y="abc"
+let z=33
+let boolIsLoggedIn=Boolean(isLoggedIn)
+let x1=Boolean(x)
+let y1=Boolean(y)
+let z1=String(z)
+console.log(boolIsLoggedIn);
+console.log(x1);
+console.log(y1);
+console.log(z1);
+
+//1=>true
+
