3014b6dc8f0e66dffc2b58d73a742463365fd941
Om_Latkar <63500839+OmLatkar@users.noreply.github.com>
2024-11-28 14:53:13 +0000

    Maths

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,24 @@
+const balance = new Number(100);
+console.log(balance.toFixed(2));
+const num=23.1234
+console.log(num.toPrecision(7));
+const x=100000
+console.log(x.toLocaleString());
+console.log(x.toLocaleString('en-IN'));
+
+
+// MATHS 
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(3.5));
+console.log(Math.round(3.4));
+console.log(Math.ceil(3.2));
+console.log(Math.floor(3.7));
+console.log(Math.max(4,3,6,8));
+console.log(Math.random()); // 0 to 1
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10+1));
+//10 to 20
+
+console.log(Math.floor(Math.random()*11+10));
+//console.log(Math.floor(Math.random()*(max-min+1)+min));
