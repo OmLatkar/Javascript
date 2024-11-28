17fe417ff679ebfd50abca98b6b05c6bdfac81e9
Om_Latkar <63500839+OmLatkar@users.noreply.github.com>
2024-11-28 13:17:41 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name="Om"
+const age=20
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const fullname=new String("Om Latkar")
+console.log(fullname[0]);
+console.log(fullname.__proto__);
+console.log(fullname.length);
+console.log(fullname.toUpperCase());
+console.log(fullname.charAt(2));
+console.log(fullname.indexOf('m'));
+console.log(fullname.substring(0,4));
+console.log(fullname.slice(-7,5));
+//trim()-removes spaces
+//.replace('str1','str2')
+//.includes('str')
+//.split('-')
